4b6a3961cf5dd69d1572fd9f82f6533f8b906104
Iden5347 <iden5347@gmail.com>
2021-10-03 09:49:07 -0700

    comit 0.1

---
+++ index.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "This is a simulator for American/Las Vegas strip blackjack, please select your rules width true/false." << endl;
+  cout << "Allow DAS: " <<;
+  cin >>
+  int values[] = {11, 10, 10, 10, 10, 9, 8, 7, 6, 5, 4, 3, 2};
+  int deck[64 * numOfDecks];
+  for()
+
+
+  return 0;
+}
